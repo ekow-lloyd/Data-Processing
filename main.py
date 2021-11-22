de7d0f63f4b0032e21df16801ea70a7d954dd0fa
eKow <ekowlloyd@gmail.com>
2021-11-22 23:40:34 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import csv
+import json
+ 
+ 
+# a function to import the data from csv - jason)
+# takes two argument
+def convert_json(csvFile, jsonFile):
+ 
+    data = {}
+ 
+    with open(csvFile, 'r') as csvf:
+        csvReader = csv.DictReader(csvf)
+ 
+        for i, row in enumerate (csvReader):
+           
+            details = {
+
+                
+                "first_name": row["First Name"],
+                "second_name": row["Last Name"],
+                "age": row["Age (Years)"],
+                "sex": row["Sex"],
+                "retired": bool(row["Retired"]),
+                "marital_status": row["Marital Status"],
+                "dependents": row["Dependants"],
+                "salary": row["Yearly Salary (£)"],
+                "pension": row["Yearly Pension (£)"],
+                "company": row["Employer Company"],
+                "commute_distance": row["Distance Commuted to Work (miles)"],
+                "Address": {
+                    "street": row["Address Street"],
+                    "city": row["Address City"],
+                    "postcode": row["Address Postcode"],
+                },
+                "Credit Card": {
+                    "start_date": row["Credit Card Start Date"],
+                    "end_date": row["Credit Card Expiry Date"],
+                    "number": row["Credit Card Number"],
+                    "ccv": row["Credit Card CVV"],
+                    "iban": row["Bank IBAN"]
+                },
+                "Vehicle": {
+                    "make": row["Vehicle Make"],
+                    "model": row["Vehicle Model"],
+                    "year": row["Vehicle Year"],
+                    "category": row["Vehicle Type"]
+
+                }
+
+            }
+
+            data.setdefault(i, details)
+    
+        
+    with open(jsonFile, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+    
+ 
+csvFile = r'acw_user_data (1).csv'
+jsonFile = r'Company_Db.json'
+ 
+convert_json(csvFile,jsonFile)
